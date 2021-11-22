8b3077898c8fe66a4d7919734f3f3d47283b6239
samuel <samuel.defosse44@gmail.com>
2021-11-22 19:28:30 +0100

    ajout exemple bar chart

--- app/src/main/java/com/example/chartapplication/BarChartActivity.java
+++ app/src/main/java/com/example/chartapplication/BarChartActivity.java
@@ -1,4 +1,51 @@
 package com.example.chartapplication;
 
-public class BartChartActivity {
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class BarChartActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+        BarChart chart = findViewById(R.id.barchart);
+
+        chart.setDrawValueAboveBar(true);
+        chart.getDescription().setEnabled(false);
+        ArrayList<BarEntry> datas = new ArrayList<>();
+
+
+        //Ajout des des données pour le graphe en baton
+        datas.add(new BarEntry(1, 945f));
+        datas.add(new BarEntry(2, 1040f));
+        datas.add(new BarEntry(3, 1133f));
+        datas.add(new BarEntry(4, 1240f));
+        datas.add(new BarEntry(5, 1369f));
+        datas.add(new BarEntry(6, 1487f));
+        datas.add(new BarEntry(7, 1501f));
+        datas.add(new BarEntry(8, 1645f));
+        datas.add(new BarEntry(9, 1578f));
+        datas.add(new BarEntry(10, 1695f));
+
+        BarDataSet bardataset = new BarDataSet(datas, "datas");
+        chart.animateY(2000); 
+
+        BarData data = new BarData(bardataset);
+        data.setBarWidth(1f);
+        bardataset.setColors(ColorTemplate.JOYFUL_COLORS);
+        chart.setData(data);
+    }
 }
--- app/src/main/java/com/example/chartapplication/MainActivity.java
+++ app/src/main/java/com/example/chartapplication/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.chartapplication;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.chartapplication.BarChartActivity;
+import com.github.mikephil.charting.charts.BarChart;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BarChart barChart = (BarChart) findViewById(R.id.barchart);
+
+        btnBarChart = findViewById(R.id.btnBarChart);
+        btnBarChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, BarChartActivity.class);
+                startActivity(I);
+            }
+        });
     }
 }
