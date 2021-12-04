03440b57f221bf3c8d0a639932fce1cea54f4672
samuel <samuel.defosse44@gmail.com>
2021-12-04 16:36:49 +0100

    ajout d une seekbar pour permettre la selection du nombre de donnees a afficher

--- app/src/main/java/com/example/chartapplication/BarChartActivity.java
+++ app/src/main/java/com/example/chartapplication/BarChartActivity.java
@@ -3,33 +3,47 @@ package com.example.chartapplication;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class BarChartActivity extends AppCompatActivity  {
+public class BarChartActivity extends AppCompatActivity implements OnSeekBarChangeListener, OnChartValueSelectedListener {
+    private BarChart chart;
+    private TextView tv;
+    private SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_chart);
-        BarChart chart = findViewById(R.id.barchart);
-
         setTitle("Populations zoo de la Flèche");
 
-        chart.setDrawValueAboveBar(true);
+        chart = findViewById(R.id.barchart);
+        chart.setOnChartValueSelectedListener(this);
+
+        tv = findViewById(R.id.tv);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+        seekBar.setProgress(5);
+
+        chart.setDrawValueAboveBar(false);
         chart.getDescription().setEnabled(false);
 
         //Paramétrage de l'axe des abscisses
@@ -38,7 +52,7 @@ public class BarChartActivity extends AppCompatActivity  {
         xAxis.setGranularity(1f);
         //Définitions des labels de l'axe des abscisses
         xAxis.setValueFormatter(new ValueFormatter() {
-            private final String[] animaux = new String[]{"Éléphants", "Girafes", "Chouettes", "Lions", "Chimpanzé"};
+            private final String[] animaux = new String[]{"Éléphants", "Girafes", "Chouettes", "Lions", "Chimpanzé", "Tigres","Antilopes","Orang-outang","Manchots","Zèbres"};
 
             @Override
             public String getFormattedValue(float value) {
@@ -53,9 +67,15 @@ public class BarChartActivity extends AppCompatActivity  {
         chart.getAxisRight().setEnabled(false);//Empêche l'affichage de l'axe des ordonnées à droite du graphique
 
 
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        tv.setText(String.valueOf(seekBar.getProgress()));
+
         ArrayList<BarEntry> animals = new ArrayList<>(); //Liste qui contient les donnés du graphique
         //Assignation de valeurs aléatoires aux données
-        for (int i = 0; i< 5; i++) {
+        for (int i = 0; i< seekBar.getProgress(); i++) {
             int valueMale = (int) ((Math.random() * 6) +1);
             int valueFemale = (int) ((Math.random() * 6) +1);
             //On ajoute les données générées précédemment à la liste qui doit les contenir
@@ -94,5 +114,26 @@ public class BarChartActivity extends AppCompatActivity  {
         System.arraycopy(ColorTemplate.MATERIAL_COLORS, 0, colors, 0, 2);
 
         return colors;
+    }
+
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
